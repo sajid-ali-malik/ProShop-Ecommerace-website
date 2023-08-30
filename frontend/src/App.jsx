9398152682fb23cc1ed3fe4545e345a2b100f613
sajid-ali-malik <thesajidali1997@gmail.com>
2023-08-31 03:12:57 +0500

    Header And Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 import "./App.css";
 
 function App() {
-  return <div>frontend</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
